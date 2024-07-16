6848c8a97f273b2684aefe8dc6f1abf445463796
Rupesh Virani <rupeshvirani01@gmail.com>
2024-07-16 16:26:56 +0530

    add index.js and connect mongodb

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const movieRoutes = require('./routes/movies');
+const errorHandler = require('./middlewares/errorHandler');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/moviesdb', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(express.json());
+app.use('/api/movies', movieRoutes);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
